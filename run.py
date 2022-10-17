255835f2bbb0011ee1c00af1fef76ec5b2657947
LUCIF3R <115425399+LordLucifer999@users.noreply.github.com>
2022-10-17 10:50:10 +0100

    Create run.py

---
+++ run.py
@@ -0,0 +1,47 @@
+#######################################################
+# Name.            :Brute Facebook (BF)
+# File             : run.py
+# Author           : Lord Lucif3r 
+Python version     : 0.4
+#######################################################
+
+############# DON'T REMOVE THIS FUNCTIONS #############
+
+
+import os 
+try:
+   import requests 
+except ImportError:
+  print('\n [\x1b[1;91m!\x1b[0m] Modul requests belum terinstall!...\n')
+  os.system('pip install requests')
+  
+try:
+     import bs4 
+except ImportError: 
+    print('\n [\x1b[1;91m!\x1b[0m] Modul Bs4 belum terinstall!...\n')
+    os.system('pip install bs4')
+    
+try:
+    import rich 
+except ImportError:
+    print('\n [\x1b[1;91m!\x1b[0m] Modul Rich belum terinstall!...\n')
+    os.system('pip install rich')
+#################################################################################
+from src.cok import Brute 
+
+if __name__ == '__main__':
+os.system("git pull");os.system("rm -rf results/OK/...");os.system("rm -rf results/CP/...");os.system("rm -rf results/IG/OK/...");os.system("rm -rf results/IG/CP/...")
+
+   # os.system("clear")
+
+   # print("please wait....")
+
+   # print()
+
+   # print("script sedang dalam perbaikan..")
+
+   # exit()
+
+    Brute()
+
+    
